a195675a7cc83a4c4fffa99f8161e7e703afbb50
mcaballol <mcaballol@redhat.com>
2019-04-12 16:55:50 -0400

    up

--- waiting-room/app.js
+++ waiting-room/app.js
@@ -5,24 +5,36 @@ const states = {
 }
 
 const waitingroom = "waiting room"
-
-var usersWaiting = []
+const events = {
+  in: {
+    join:'join',
+    disconnect:'disconnect'
+  },
+  out:{
+    new_player:'user connected',
+    players_room: 'players in room',
+    disconnected: 'disconnected',
+    room_assigned 'room assigned'
+  }
+}
+const maxplayersroom=4;
 
 const app = require('http').createServer();
 const io = require('socket.io')(app);
 const fs = require('fs');
 const uuidv4 = require('uuid/v4');
+
 app.listen(8080);
 
 io.on('connection', (socket) => {
   socket.emit('news', { hello: 'world' });
-  socket.on('join', (data) => {
+  socket.on(events.in.join, (data) => {
     socket.join(waitingroom);
-    io.to(waitingroom).emit('user connected', data.token);
+    io.to(waitingroom).emit(events.out.new_player, data.token);
     updateRoom();
   });
-  socket.on('disconnect', function () {
-    socket.emit('disconnected');
+  socket.on(events.in.disconnect, function () {
+    socket.emit(events.out.disconnected);
     updateRoom();
   });
 });
@@ -30,9 +42,9 @@ io.on('connection', (socket) => {
 
 function updateRoom(){
   var room = io.sockets.adapter.rooms[waitingroom];
-  io.to(waitingroom).emit('users in room', room );
+  io.to(waitingroom).emit(events.out.players_room, room );
 
-  if (room.length==4)
+  if (room.length==maxplayersroom)
   assignGameRoom();
 }
 
@@ -46,7 +58,7 @@ return room;
 function assignGameRoom(){
 var room = createGameRoom();
 var response =  { state: states.assigned , game_room_token: room }
-io.to(waitingroom).emit('go to your game room', response);
+io.to(waitingroom).emit(events.out.room_assigned, response);
 
 //io.sockets.clients(waitingroom).forEach(function(s){
     //s.leave(waitingroom);
