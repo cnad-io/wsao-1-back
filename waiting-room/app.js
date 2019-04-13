fe2172bbc67b4bb7a995fefb6428588dfb29a031
mcaballol <mcaballol@redhat.com>
2019-04-12 21:15:59 -0400

    up

--- waiting-room/app.js
+++ waiting-room/app.js
@@ -50,7 +50,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const adminSocket = ioOut('http://game-room.wsao.svc.cluster.local:8080');
+const adminSocket = ioOut('http://game-room-internal:8081');
 
 
 
@@ -62,36 +62,21 @@ function updateRoom(){
   createGameRoom();
 }
 
-//deprecated
+
 function createGameRoom(){
 // create game room using game room service
-
-adminSocket.on('connect', function(){
-  io.to(waitingroom).emit("news", {info:"creando coneccion game-room"});
-});
-adminSocket.on('error', (error) => {
-
-  io.to(waitingroom).emit("news", {info:"error conectando", error: error});
-});
 adminSocket.on(serverEvents.in.new_room, (data) => {
   assignGameRoom(data.game_room_token);
 });
 
-adminSocket.connect()
-io.to(waitingroom).emit("news", {info: adminSocket.connected});
 io.to(waitingroom).emit("news", {info:"creando game-room"});
 adminSocket.emit(serverEvents.out.createRoom, {});
 
-
-//var room = uuidv4();
-
-//return room;
 }
 
 function assignGameRoom(game_room_token){
 
   var response =  { state: states.assigned , game_room_token: game_room_token }
   io.to(waitingroom).emit(events.out.room_assigned, response);
-
-io.to(waitingroom).emit("news", {info:"game-room:"+game_room_token});
+  io.to(waitingroom).emit("news", {info:"game-room:"+game_room_token});
 }
