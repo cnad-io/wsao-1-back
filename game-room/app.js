54b9793709c7230ee3b4ced57ec751e98229494d
mcaballol <mcaballol@redhat.com>
2019-04-12 21:21:12 -0400

    up

--- game-room/app.js
+++ game-room/app.js
@@ -14,7 +14,7 @@ const serverEvents = {
 const app = require('http').createServer();
 const appint = require('http').createServer();
 const io = require('socket.io')(app);
-const ioint = require('socket.io')(app);
+const ioint = require('socket.io')(appint);
 
 const fs = require('fs');
 const uuidv4 = require('uuid/v4');
@@ -26,6 +26,16 @@ appint.listen(8081);
 
 
 ioint.on('connection', (socket) => {
+  socket.on(serverEvents.in.createRoom, (data) => {
+      var game_room_token= uuidv4();
+      socket.emit(serverEvents.out.new_room, { game_room_token: game_room_token });
+
+  });
+
+});
+
+
+io.on('connection', (socket) => {
   socket.on(serverEvents.in.createRoom, (data) => {
       var game_room_token= uuidv4();
       socket.emit(serverEvents.out.new_room, { game_room_token: game_room_token });
