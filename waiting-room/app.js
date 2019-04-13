f4bfd738028a2b79eba911a7930ddff71ba3b17e
mcaballol <mcaballol@redhat.com>
2019-04-12 20:21:53 -0400

    up

--- waiting-room/app.js
+++ waiting-room/app.js
@@ -56,30 +56,29 @@ function updateRoom(){
   io.to(waitingroom).emit(events.out.players_room, room );
 
   if (room.length==maxplayersroom)
-  assignGameRoom();
+  createGameRoom();
 }
 
 //deprecated
 function createGameRoom(){
 // create game room using game room service
 
+adminSocket.on(serverEvents.in.new_room, (data) => {
+  assignGameRoom(data.game_room_token);
+});
+io.to(waitingroom).emit("news", {info:"creando game-room"});
+adminSocket.emit(serverEvents.out.createRoom, {});
 
 
-var room = uuidv4();
+//var room = uuidv4();
 
-return room;
+//return room;
 }
 
-function assignGameRoom(){
+function assignGameRoom(game_room_token){
 
-  adminSocket.on(serverEvents.in.new_room, (data) => {
-    var response =  { state: states.assigned , game_room_token: data.game_room_token }
-    io.to(waitingroom).emit(events.out.room_assigned, response);
+  var response =  { state: states.assigned , game_room_token: game_room_token }
+  io.to(waitingroom).emit(events.out.room_assigned, response);
 
-  });
-  adminSocket.emit(serverEvents.out.createRoom, {});
-//io.sockets.clients(waitingroom).forEach(function(s){
-    //s.leave(waitingroom);
-//});
-io.to(waitingroom).emit("news", {});
+io.to(waitingroom).emit("news", {info:"game-room:"+game_room_token});
 }
